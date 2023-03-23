ea7a90d53d404666abd657ec0affb99482fb94a8
Thirvin <leesin5202@gmail.com>
2023-03-23 23:59:18 +0800

    add path

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,13 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+
     app.config['SECRET_KEY'] = 'aaabbbcdefg'
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views,url_prfix = '/')
+    app.register_blueprint(auth,url_prfix = '/auth/')
 
     return app
