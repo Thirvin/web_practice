b294176a7be4e21648b72236ebcd850889e75412
Thirvin <leesin5202@gmail.com>
2023-03-24 21:52:22 +0800

    [add] get data in login and sigh-up

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,18 @@
-from flask import Blueprint,render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth',__name__)
 
-@auth.route("/login")
+@auth.route("/login",methods =['GET','POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html")
 
 @auth.route("/logout")
 def logout():
+    data = request.form
     return render_template("logout.html")
 
-@auth.route("/sigh-up")
+@auth.route("/sigh-up",methods =['GET','POST'])
 def register():
     return render_template("sigh-up.html")
