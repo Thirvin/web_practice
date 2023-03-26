4aec207d3acebcf3f94f22a907a2a3eb23d99ab3
Thirvin <leesin5202@gmail.com>
2023-03-26 22:51:27 +0800

    [add] data base

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 def create_app():
     app = Flask(__name__)
-
     app.config['SECRET_KEY'] = 'aaabbbcdefg'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
 
     from .views import views
     from .auth import auth
@@ -11,4 +20,16 @@ def create_app():
     app.register_blueprint(views,url_prfix = '/')
     app.register_blueprint(auth,url_prfix = '/auth/')
 
+
+    from .models import User,Note
+    with app.app_context():
+        db.create_all()
+
+
+
     return app
+
+def create_database(app):
+    if not path.exists('website/'+DB_NAME):
+        db.create_all(app = app)
+        print("data base created successfully")
